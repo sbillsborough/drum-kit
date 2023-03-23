5d5eb2907df184d65238293f1450a1d3a3d946f9
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-03-23 17:03:22 +0000

    linked the index.js with defer loading. Added querySelector to the first button and stored in tomOne variable. Added eventLIstener to tomOne and a clickHandler function with an alert to test functionality

---
+++ index.js
@@ -0,0 +1,12 @@
+// Buttons
+var tomOne = document.querySelector("button");
+
+// Click Handler Function
+
+function clickHandler() {
+  alert("clicked!");
+}
+
+// Event Listeners
+
+tomOne.addEventListener("click", clickHandler);
